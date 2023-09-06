7659ca2bd4806c7a5d40c3a8e60c599f33ef3670
vivekneupane11 <vivekneupane11@gmail.com>
2023-09-06 14:24:55 +0545

    Banner added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import BannerSlider from "@/components/BannerSlider";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
 <main>
+<BannerSlider/>
+
   <h1 >HI</h1>
   <h1 >HI</h1>
   <h1 >HI</h1>
---
+++ components/BannerSlider/index.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable @next/next/no-img-element */
+"use client"
+import banner from '@/public/bannerHome.png';
+import bannertwo from '@/public/bannertwo.png';
+import Image from 'next/image';
+import Slider from "react-slick";
+import styles from './bannerslider.module.css';
+export default function BannerSlider() {
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 2000,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      };
+    
+      return (
+        <Slider {...settings}>
+          <div className={styles.sliderBanner}>
+            <div className={
+                styles.banner 
+            }>
+              <section className={
+                styles.bannerLeft
+              }>
+                <article>
+                  <strong>new</strong>
+                  <span>New Arrivals</span>
+                  <h6 className={styles.bannerHeader} >New Designs Affordable Price</h6>
+                  <button className={styles.bannerButton}>View Products</button>
+                </article>
+                <Image src={bannertwo}  className={styles.bannerImageTwo} alt="Banner" />
+              </section>
+              <section className={
+                styles.bannerRight
+              }>
+                <article>
+                  <strong>new</strong>
+                  <span>New Arrivals</span>
+                  <h6 className={styles.bannerHeader} >New Designs with Affordable Price</h6>
+                  <button className={styles.bannerButton}>View Products</button>
+                </article>
+                <Image src={banner}  className={styles.bannerImages} alt="Banner" />
+              </section>
+            </div>
+          </div>
+          <div className={styles.sliderBanner}>
+            <div className={styles.banner}>
+              <section className={styles.bannerLeft}>
+                <article>
+                  <strong>new</strong>
+                  <span>New Arrivals</span>
+                  <h6 className={styles.bannerHeader} >New Designs Affordable Price</h6>
+                  <button className={styles.bannerButton}>View Products</button>
+                </article>
+                <Image src={bannertwo}  className={styles.bannerImageTwo} alt="Banner" />
+              </section>
+              <section className={styles.bannerRight}>
+                <article>
+                  <strong>new</strong>
+                  <span>New Arrivals</span>
+                  <h6 className={styles.bannerHeader} >New Designs with Affordable Price</h6>
+                  <button className={styles.bannerButton}>View Products</button>
+                </article>
+                <Image src={banner}  className={styles.bannerImages} alt="Banner" />
+              </section>
+            </div>
+          </div>{" "}
+          <div className={styles.sliderBanner}>
+            <div className={
+                styles.banner
+            }>
+              <section className={styles.bannerLeft}>
+                <article>
+                  <strong>new</strong>
+                  <span>New Arrivals</span>
+                  <h6 className={styles.bannerHeader} >New Designs Affordable Price</h6>
+                  <button className={styles.bannerButton}>View Products</button>
+                </article>
+                <Image src={banner}  className={styles.bannerImages} alt="Banner" />
+              </section>
+              <section className={styles.bannerRight}>
+                <article>
+                  <strong>new</strong>
+                  <span>New Arrivals</span>
+                  <h6 className={styles.bannerHeader} >New Designs with Affordable Price</h6>
+                  <button className={styles.bannerButton}>View Products</button>
+                </article>
+                <Image src={bannertwo}  className={styles.bannerImageTwo} alt="Banner" />
+              </section>
+            </div>
+          </div>
+        </Slider>
+      );
+}
