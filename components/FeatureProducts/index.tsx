dfd9d6e71e3c44f71a5bbe515a1a1247d1afceb3
vivekneupane11 <vivekneupane11@gmail.com>
2023-09-10 18:47:21 +0545

    home page responsive

--- components/FeatureProducts/index.tsx
+++ components/FeatureProducts/index.tsx
@@ -27,6 +27,56 @@ export default function FeatureProducts({ title }: { title: string }) {
     speed: 900,
     slidesToShow: 5,
     slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: false,
+          center:true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          centerMode: false,
+
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide:1,
+          centerMode: true,
+          
+        }
+      }
+    ]
   };
   return (
     <section className={styles.featureProducts}>
