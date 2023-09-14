19895b6cf34ece9abb5ee9bcd53f370e536f5c46
vivekneupane11 <vivekneupane11@gmail.com>
2023-09-14 16:45:04 +0545

    wip

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -133,11 +133,11 @@ export default function Product({ params }: { params: { id: string } }) {
               </div>
               <div className={styles.description}>
                 <strong>
-                  <strong>Descriptions : </strong>There a wooden gbox that comes
-                  out with the watchThere a wooden golden plate box that comes
-                  out with the watchThere a wooden golden plate box that comes
-                  out with the watchThere a wooden golden plated shinged color
-                  box that comes out with the watch.
+                  <b>Descriptions : </b>There a wooden gbox that comes out with
+                  the watchThere a wooden golden plate box that comes out with
+                  the watchThere a wooden golden plate box that comes out with
+                  the watchThere a wooden golden plated shinged color box that
+                  comes out with the watch.
                 </strong>
               </div>
               <div className={styles.varients}>
@@ -246,7 +246,23 @@ export default function Product({ params }: { params: { id: string } }) {
                 <li>Reviews</li>
               </ul>
 
-              <span className={styles.shareButton}></span>
+              <span className={styles.shareButton}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="#fff"
+                  height={25}
+                  width={25}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+                  />
+                </svg>
+              </span>
             </div>
             <div className={styles.reviewContainer}>
               <ul>
