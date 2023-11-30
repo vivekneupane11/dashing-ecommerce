704be424ba8d4f835582f65aed7aca8afa9d15c5
vivekneupane11 <vivekneupane11@gmail.com>
2023-11-30 13:37:48 +0545

    auth option type fixed

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,8 @@
 import Button from "@/components/Button";
-import { getServerSession } from "next-auth";
+import { NextAuthOptions, getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Profile() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions as NextAuthOptions);
   return <>{session ? <Button type="signout" /> : <Button type="signin" />}</>;
 }
